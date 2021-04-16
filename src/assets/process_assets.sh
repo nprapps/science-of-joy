c235a7bc0fe475ab39ff4dbbfae3a1680dcc0188
thomaswilburn <twilburn@npr.org>
2021-04-16 10:04:05 -0500

    Add image processing

--- src/assets/process_assets.sh
+++ src/assets/process_assets.sh
@@ -3,8 +3,8 @@
 # by default this only processes new files
 # if you want to update an existing file, delete the output first
 
-# set to "info" for more loggin
-ffmpeg_log="error"
+# set to "error" to suppress logs
+ffmpeg_log="info"
 
 # enter the originals directory
 cd originals
@@ -12,24 +12,30 @@ cd originals
 # handle silenced video
 mkdir -p ../synced/silent
 for video in silent/*.mp4; do
+  echo "Processing $video..."
+
   # create the videos
-  ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
-  -i $video \
-  -an \
-  -vcodec libx264 \
-  -preset veryslow \
-  -strict -2 \
-  -pix_fmt yuv420p \
-  -crf 30 \
-  -vf scale=640:-2 \
-  ../synced/$video
+  if [ ! -f  ../synced/$video ]; then
+    ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
+    -i $video \
+    -an \
+    -vcodec libx264 \
+    -preset veryslow \
+    -strict -2 \
+    -pix_fmt yuv420p \
+    -crf 30 \
+    -vf scale=640:-2 \
+    ../synced/$video
+  fi
 
   # create posters
-  ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
-  -i $video \
-  -qscale:v 4 \
-  -frames:v 1 \
-  ../synced/$video.jpg
+  if [ ! -f ../synced/$video.jpg ]; then
+    ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
+    -i $video \
+    -qscale:v 4 \
+    -frames:v 1 \
+    ../synced/$video.jpg
+  fi
 
 done
 
@@ -38,48 +44,55 @@ done
 # transcode video with sound
 mkdir -p ../synced/video
 for video in video/*.mp4; do
-  ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
-  -i $video \
-  -vcodec libx264 \
-  -preset veryslow \
-  -strict -2 \
-  -pix_fmt yuv420p \
-  -crf 30 \
-  -vf scale=640:-2 \
-  ../synced/$video
+  echo "Processing $video..."
+
+  if [ ! -f  ../synced/$video ]; then
+    ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
+    -i $video \
+    -vcodec libx264 \
+    -preset veryslow \
+    -strict -2 \
+    -pix_fmt yuv420p \
+    -crf 30 \
+    -vf scale=640:-2 \
+    ../synced/$video
+  fi
 
   # create posters
-  ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
-  -i $video \
-  -qscale:v 4 \
-  -frames:v 1 \
-  ../synced/$video.jpg
+  if [ ! -f  ../synced/$video.jpg ]; then
+    ffmpeg -n -nostats -hide_banner -loglevel $ffmpeg_log \
+    -i $video \
+    -qscale:v 4 \
+    -frames:v 1 \
+    ../synced/$video.jpg
+  fi
 done
 
 # processing audio into MP3
 mkdir -p ../synced/audio
 for audio in audio/*.wav; do
-  if [ -f "../synced/$audio.mp3" ]; then
-    echo "../synced/$audio.mp3 already exists"
-    continue
+  echo "Processing $audio..."
+  if [ ! -f "../synced/$audio.mp3" ]; then
+    lame -m s -b 128 "$audio" "../synced/$audio.mp3"
   fi
-  lame -m s -b 128 "$audio" "../synced/$audio.mp3"
 done
 
 for audio in audio/*.mp3; do
-  if [ -f "../synced/$audio.mp3" ]; then
-    echo "../synced/$audio.mp3 already exists"
-    continue
+  echo "Processing $audio..."
+  if [ ! -f "../synced/$audio.mp3" ]; then
+    lame -m s -b 128 "$audio" "../synced/$audio.mp3"
   fi
-  lame -m s -b 128 "$audio" "../synced/$audio.mp3"
 done
 
-# convert parallax images
+# convert images
 # requires ImageMagick 7
-# mkdir -p ../synced/parallax
-# for img in audio/*.png; do
-#   magick convert $img -resize 1000x800\> -colors 255 ../synced/$img
-# done
+mkdir -p ../synced/images
+for img in images/*; do
+  echo "Processing $img..."
+  if [ ! -f "../synced/$img" ]; then
+    magick convert $img -resize 1200x800\> ../synced/$img
+  fi
+done
 
 # exit back to assets
 cd ..
