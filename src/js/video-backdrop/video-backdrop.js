00790bc2d0aa995d4555bdda04d30543cec1ef86
thomaswilburn <twilburn@npr.org>
2021-06-04 15:28:22 -0500

    Single-buffer video-backdrop on Safari

--- src/js/video-backdrop/video-backdrop.js
+++ src/js/video-backdrop/video-backdrop.js
@@ -1,32 +1,54 @@
 var CustomElement = require("../customElement");
 
-class VideoBackdrop extends CustomElement {
+var isSafari = !!navigator.userAgent.match(/i(os|pad|phone)/i);
 
-  static template = require("./_video-backdrop.html")
-  static boundMethods = []
+class VideoBackdrop extends CustomElement {
+  static template = require("./_video-backdrop.html");
+  static boundMethods = [];
 
   constructor() {
     super();
 
-    this.buffers = [
-      this.elements.bufferA,
-      this.elements.bufferB
-    ];
+    this.buffers = [this.elements.buffer];
+    var [ front ] = this.buffers;
+    if (!isSafari) {
+      var cloned = front.cloneNode();
+      this.elements.videoContainer.appendChild(cloned);
+      this.buffers.push(cloned);
+    }
   }
 
-  static observedAttributes = ["src", "poster", "cuesrc", "cueposter", "loop", "autoplay", "muted"]
-  static mirroredProps = ["src", "poster", "cuesrc", "cueposter", "loop", "autoplay", "muted"];
+  static observedAttributes = [
+    "src",
+    "poster",
+    "cuesrc",
+    "cueposter",
+    "loop",
+    "autoplay",
+    "muted"
+  ];
+  static mirroredProps = [
+    "src",
+    "poster",
+    "cuesrc",
+    "cueposter",
+    "loop",
+    "autoplay",
+    "muted"
+  ];
 
   attributeChangedCallback(attr, was, value) {
-    var [ front, back ] = this.buffers;
+    var [front, back] = this.buffers;
     var present = value != null;
     switch (attr) {
       case "cuesrc":
-          back.src = value;
-          back.pause();
+        if (isSafari) return;
+        back.src = value;
+        back.pause();
         break;
 
       case "cueposter":
+        if (isSafari) return;
         back.poster = value;
         break;
 
@@ -41,34 +63,31 @@ class VideoBackdrop extends CustomElement {
           return;
         }
         // check to see if this video was cued up
-        if (value == back.getAttribute("src")) {
+        if (!isSafari && value == back.getAttribute("src")) {
           // console.log(`Swapping buffers for ${value}`);
           // swap buffers
           front.classList.remove("front");
-          back.classList.add("front");
-          this.buffers = [ back, front ];
+          this.buffers = [back, front];
           front = back;
         } else {
           // console.log(`Uncued src: ${value}`);
-          front.classList.add("front");
           front.src = value;
         }
+        front.classList.add("front");
         front.play();
         break;
 
       case "loop":
-        front.loop = present;
-        back.loop = present;
+        this.buffers.forEach(b => b.loop = present);
         break;
 
       case "autoplay":
-        front.autoplay = present;
-        back.autoplay = present;
+        this.buffers.forEach(b => b.autoplay = present);
         break;
 
       case "muted":
-        front.muted = present;
-        back.muted = present;
+        this.buffers.forEach(b => b.muted = present);
+        break;
     }
   }
 
@@ -77,9 +96,8 @@ class VideoBackdrop extends CustomElement {
   }
 
   set currentTime(t) {
-    return this.buffers[0].currentTime = t;
+    return (this.buffers[0].currentTime = t);
   }
-
 }
 
 VideoBackdrop.define("video-backdrop");
