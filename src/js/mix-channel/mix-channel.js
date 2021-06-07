b282addad0445a8fdc05d8834c746bab8b2c185e
thomaswilburn <twilburn@npr.org>
2021-06-07 12:10:41 -0500

    Use WebAudio to process mix-channel inputs
    Fixes #79
    Safari is THE WORST

--- src/js/mix-channel/mix-channel.js
+++ src/js/mix-channel/mix-channel.js
@@ -1,54 +1,69 @@
 var CustomElement = require("../customElement.js");
 
+
+var context = "webkitAudioContext" in window ? new webkitAudioContext() : new AudioContext();
+
 class MixChannel extends CustomElement {
-	static template = require("./_mix-channel.html");
-	static boundMethods = ["onPlay", "onSlide"];
-	constructor() {
-		super();
-		var { audio, play, slider } = this.elements;
-		//audio.src = './assets/synced/audio/soundscape_tech.mp3.mp3'
-		play.addEventListener("click", this.onPlay);
-		slider.addEventListener("input", this.onSlide);
-		slider.addEventListener("touchstart", this.onSlide);
-		slider.addEventListener("mousedown", this.onSlide);
-		document.body.addEventListener("webstorypage", (d) => {
-			audio.pause();
-			play.setAttribute("aria-pressed", "false");
-			slider.style.opacity = 1;
-			audio.volume = 0;
-		});
-	}
+  static template = require("./_mix-channel.html");
+  static boundMethods = ["onPlay", "onSlide"];
+  constructor() {
+    super();
+    var { audio, play, slider } = this.elements;
+    var source = (this.source = context.createMediaElementSource(audio));
+    this.amp = context.createGain();
+    this.amp.connect(context.destination);
+    this.source.connect(this.amp);
+    //audio.src = './assets/synced/audio/soundscape_tech.mp3.mp3'
+    play.addEventListener("click", this.onPlay);
+    slider.addEventListener("input", this.onSlide);
+    slider.addEventListener("touchstart", this.onSlide);
+    slider.addEventListener("mousedown", this.onSlide);
+    document.body.addEventListener("webstorypage", (d) => {
+      audio.pause();
+      play.setAttribute("aria-pressed", "false");
+      slider.style.opacity = 1;
+      this.amp.gain.value = 0;
+      context.suspend();
+      slider.value = 0;
+    });
+  }
 
-	static observedAttributes = ["src"];
-	attributeChangedCallback(attr, was, value) {
-		switch (attr) {
-			case "src":
-				this.elements.audio.src = value;
-				break;
-		}
-	}
+  static observedAttributes = ["src"];
+  attributeChangedCallback(attr, was, value) {
+    switch (attr) {
+      case "src":
+        this.elements.audio.src = value;
+        break;
+    }
+  }
 
-	onSlide() {
-		var { audio, slider, play } = this.elements;
-		if (audio.paused) audio.play();
-		audio.volume = slider.value * 1 + .01;
-		if (slider.value > 0) {
-			play.setAttribute("aria-pressed", "true");
-		} else {
-			play.setAttribute("aria-pressed", "false");
-		}
-	}
+  onSlide() {
+    var { audio, slider, play } = this.elements;
+    if (audio.paused) audio.play();
+    context.resume();
+    this.amp.gain.value = slider.valueAsNumber;
+    if (slider.value > 0) {
+      play.setAttribute("aria-pressed", "true");
+    } else {
+      play.setAttribute("aria-pressed", "false");
+    }
+  }
 
-	onPlay() {
-		var { audio, play, slider } = this.elements;
-		if (audio.paused) {
-			audio.play();
-			play.setAttribute("aria-pressed", "true");
-		} else {
-			audio.pause();
-			play.setAttribute("aria-pressed", "false");
-		}
-	}
+  onPlay() {
+    var { audio, play, slider } = this.elements;
+    context.resume();
+    if (audio.paused) {
+      audio.play();
+      play.setAttribute("aria-pressed", "true");
+      if (this.elements.slider.valueAsNumber == 0) {
+        this.elements.slider.value = .5;
+        this.amp.gain.value = .5;
+      }
+    } else {
+      audio.pause();
+      play.setAttribute("aria-pressed", "false");
+    }
+  }
 }
 
 MixChannel.define("mix-channel");
