e65fbdab4ae3952c48bab138bb13d90dd068a64f
Shajia Abidi <sabidi@npr.org>
2024-08-01 16:00:13 -0700

    ga4 update

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,89 +1,111 @@
-var DataConsent = require('./lib/data-consent');
-
+var DataConsent = require("./lib/data-consent");
 var googleAnalyticsAlreadyInitialized = false;
 
-var setupGoogleAnalytics = function() {
+var setupGoogleAnalytics = function () {
+  if (window.top !== window) {
+    var gtagID = "G-LLLW9F9XPC";
+  } else {
+    var gtagID = "G-XK44GJHVBE";
+  }
   // Bail early if opted out of Performance and Analytics consent groups
-  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
+  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS))
+    return;
 
+  var script = document.createElement("script");
 
-  
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + gtagID;
 
-  if (window.top !== window) {  
-    ga("create", "UA-5828686-75", "auto");
+  script.async = true;
+
+  var script_embed = document.createElement("script");
+
+  script_embed.innerHTML =
+    "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '" +
+    gtagID +
+    "', { 'send_page_view': false });";
+  document.head.append(script, script_embed);
+
+  if (window.top !== window) {
     // By default Google tracks the query string, but we want to ignore it.
     var here = new URL(window.location);
 
-    ga("set", "location", here.protocol + "//" + here.hostname + here.pathname);
-    ga("set", "page", here.pathname);
-
     // Custom dimensions & metrics
-    var parentUrl = here.searchParams.has("parentUrl") ? new URL(here.searchParams.get("parentUrl")) : "";
+    var parentUrl = here.searchParams.has("parentUrl")
+      ? new URL(here.searchParams.get("parentUrl"))
+      : "";
     var parentHostname = "";
 
     if (parentUrl) {
-        parentHostname = parentUrl.hostname;
+      parentHostname = parentUrl.hostname;
     }
 
     var initialWidth = here.searchParams.get("initialWidth") || "";
 
-    ga("set", {
-      dimension1: parentUrl,
-      dimension2: parentHostname,
-      dimension3: initialWidth
-    });
-} else {
-  ga("create", "UA-5828686-4", "auto");
+    var customData = {};
+    customData["dimension1"] = parentUrl;
+    customData["dimension2"] = parentHostname;
+    customData["dimension3"] = initialWidth;
+  } else {
+    // Secondary topics
+    var dim6 = "";
+    // Topic IDs
+    var dim2 = "";
 
-  // Secondary topics
-  var dim6 = "";
-  // Topic IDs
-  var dim2 = "";
+    // Google analytics doesn't accept arrays anymore, these must be strings.
 
-  // Google analytics doesn't accept arrays, these must be strings.
-
-  if (window.PROJECT_ANALYTICS.secondaryTopics) {
     try {
       dim6 = window.PROJECT_ANALYTICS.secondaryTopics.join(", ");
     } catch (error) {
-      console.log("PROJECT_ANALYTICS.secondaryTopics is not an array, check project.json");
+      console.log(
+        "PROJECT_ANALYTICS.secondaryTopics is not an array, check project.json"
+      );
     }
-  }
 
-  if (window.PROJECT_ANALYTICS.topicIDs) {
     try {
       dim2 = window.PROJECT_ANALYTICS.topicIDs.join(", ");
     } catch (error) {
-      console.log("PROJECT_ANALYTICS.topicIDs is not an array, check project.json");
+      console.log(
+        "PROJECT_ANALYTICS.topicIDs is not an array, check project.json"
+      );
     }
+
+    var customData = {};
+    customData["dimension2"] = dim2;
+    customData["dimension3"] = window.PROJECT_ANALYTICS
+      ? window.PROJECT_ANALYTICS.primaryTopic
+      : "News";
+    customData["dimension6"] = dim6;
+    customData["dimension22"] = document.title;
   }
 
-  ga("set", {
-    dimension2: dim2,
-    dimension3: (window.PROJECT_ANALYTICS.primaryTopic || "News"),
-    dimension6: dim6,
-    dimension22: document.title
-  });
-}
-  ga("send", "pageview");
+  gtag("event", "page_view", customData);
   googleAnalyticsAlreadyInitialized = true;
-}
+};
 
 // Add GA initialization to window.onload
-var oldOnload = window.onload;
-window.onload = (typeof window.onload != 'function') ? setupGoogleAnalytics : function() { oldOnload(); setupGoogleAnalytics(); };
+function addLoadEvent(func) {
+  var oldOnLoad = window.onload;
+  if (typeof window.onload != "function") {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      if (oldOnLoad) {
+        oldOnLoad();
+      }
+      func();
+    };
+  }
+}
 
-// Listen for DataConsentChanged event 
-document.addEventListener('npr:DataConsentChanged', () => {
-  // Bail early if it's already been set up 
+addLoadEvent(setupGoogleAnalytics);
+
+// Listen for DataConsentChanged event
+document.addEventListener("npr:DataConsentChanged", () => {
+  // Bail early if it's already been set up
   if (googleAnalyticsAlreadyInitialized) return;
 
   // When a user opts into performance and analytics cookies, initialize GA
   if (DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) {
     setupGoogleAnalytics();
-   }
+  }
 });
--- src/js/lib/tracking.js
+++ src/js/lib/tracking.js
@@ -6,32 +6,34 @@
 @param [label] - not usually visible in dashboard, defaults to title or URL
 */
 
-var DIMENSION_PARENT_URL = 'dimension1';
-var DIMENSION_PARENT_HOSTNAME = 'dimension2';
-var DIMENSION_PARENT_INITIAL_WIDTH = 'dimension3';
+var DataConsent = require("./data-consent");
+
+var DIMENSION_PARENT_URL = "dimension1";
+var DIMENSION_PARENT_HOSTNAME = "dimension2";
+var DIMENSION_PARENT_INITIAL_WIDTH = "dimension3";
 
 var a = document.createElement("a");
-var DataConsent = require('./data-consent');
 
 var slug = window.location.pathname.replace(/^\/|\/$/g, "");
 
-var track = function(eventAction, eventLabel, eventValue) {
+var track = function (eventAction, eventLabel, eventValue = "") {
   // Bail early if opted out of Performance and Analytics consent groups
-  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS)) return;
+  if (!DataConsent.hasConsentedTo(DataConsent.PERFORMANCE_AND_ANALYTICS))
+    return;
 
   var event = {
     eventAction,
     eventLabel,
     eventValue,
     hitType: "event",
-    eventCategory: "apps-" + slug
-  }
+    eventCategory: "apps-" + slug,
+  };
 
-  console.log(`Tracking: ${eventAction} / ${eventLabel} / ${eventValue}`)
+  // console.log(`Tracking: ${eventAction} / ${eventLabel} / ${eventValue}`);
 
   var search = window.location.search.replace(/^\?/, "");
   var query = {};
-  search.split("&").forEach(pair => {
+  search.split("&").forEach((pair) => {
     var [key, value] = pair.split("=");
     query[key] = value;
   });
@@ -42,7 +44,13 @@ var track = function(eventAction, eventLabel, eventValue) {
   event[DIMENSION_PARENT_URL] = parentURL;
   event[DIMENSION_PARENT_HOSTNAME] = hostname;
 
-  if (window.ga) ga("send", event);
+  // if (window.ga) ga("send", event);
+  if (window.gtag)
+    gtag("event", slug, {
+      type: eventAction,
+      label: eventLabel,
+      value: eventValue,
+    });
 };
 
 module.exports = track;
